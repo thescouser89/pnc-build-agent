93e76be94598592784e4edf319f4a5ca01bffd39
Matej Lazar <matejonnet@gmail.com>
2019-05-05 11:17:14 +0200

    Add sequnce number to json fomatted log.

---
+++ server/src/main/java/org/jboss/pnc/buildagent/server/formatter/SequencedLayout.java
@@ -0,0 +1,19 @@
+package org.jboss.pnc.buildagent.server.formatter;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @author <a href="mailto:matejonnet@gmail.com">Matej Lazar</a>
+ */
+public class SequencedLayout extends CustomKeysJsonLayout {
+
+    private static final AtomicLong sequence = new AtomicLong();
+
+    protected void addCustomDataToJsonMap(Map<String, Object> map, ILoggingEvent event) {
+        map.put("sequence", sequence.getAndIncrement());
+    }
+
+}
