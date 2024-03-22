bcae2569bbcc0cc924347e7b73fccac194331754
Dustin Kut Moy Cheung <dcheung@redhat.com>
2024-03-22 13:33:32 -0400

    [NCL-8159] Set partition key for kafka message based on mdc.processContext value

--- server/src/main/java/org/jboss/pnc/buildagent/server/KafkaQueueAdapter.java
+++ server/src/main/java/org/jboss/pnc/buildagent/server/KafkaQueueAdapter.java
@@ -3,11 +3,14 @@ package org.jboss.pnc.buildagent.server;
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.jboss.pnc.api.constants.MDCKeys;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 import java.time.Duration;
 import java.util.Properties;
+import java.util.Random;
 import java.util.function.Consumer;
 
 /**
@@ -15,6 +18,18 @@ import java.util.function.Consumer;
  */
 public class KafkaQueueAdapter implements QueueAdapter {
 
+    /**
+     * This is the default key we'll use for sending messages to Kafka if the processKey of the message is not specified
+     * and will stay consistent while the app is running!
+     *
+     * Why is it the same for the duration the app is running? So that all the logs go to the same partition in the
+     * Kafka topic, to preserve order
+     *
+     * Why is it randomly generated at the beginning? So that when we select a random partition at the beginning, rather
+     * than sending messages to the same partition all the time and potentially creating a hotspot when all the
+     * build-agent instances run at the same time.
+     */
+    private static final String DEFAULT_KEY = String.valueOf(new Random().nextInt(8192));
     private static final Logger log = LoggerFactory.getLogger(KafkaQueueAdapter.class);
 
     private final KafkaProducer kafkaProducer;
@@ -32,7 +47,15 @@ public class KafkaQueueAdapter implements QueueAdapter {
 
     @Override
     public void send(String message, Consumer<Exception> exceptionHandler) {
-        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(queueTopic, message);
+
+        String key = MDC.get(MDCKeys.PROCESS_CONTEXT_KEY);
+
+        if (key == null || key.isEmpty()) {
+            // fallback key to use in case processContext is not specified in the MDC
+            key = DEFAULT_KEY;
+        }
+
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(queueTopic, key, message);
         Callback callback = (metadata, exception) -> {
             if (exception != null) {
                 exceptionHandler.accept(exception);
